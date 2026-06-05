6ae306bb14824949e135643ed7c0cdc482122107
SystemSamurai <GitHub@systemsamurai.com>
2026-06-05 07:30:30 -0400

    ch2_l4

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from logger import log_state
 
 
 def main() -> None:
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+#    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+#    print(f"Screen width: {SCREEN_WIDTH}")
+#    print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0.0
 
     while True:
         log_state()
@@ -20,6 +23,9 @@ def main() -> None:
 
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+#        print ({dt})
 
 
 if __name__ == "__main__":
